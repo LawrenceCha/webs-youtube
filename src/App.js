e00e4d61087f712434926ba9a94ab0bd8c3b37c1
LawrenceCha <127981281+LawrenceCha@users.noreply.github.com>
2024-04-12 16:40:55 +0900

    add section components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Developer from './pages/Developer'
+import Webd from './pages/Webd'
+import Website from './pages/Website'
+import Gsap from './pages/Gsap'
+import Port from './pages/Port'
+import Youtube from './pages/Youtube'
+import Channel from './pages/Channel'
+import Video from './pages/Video'
+import Search from './pages/Search'
+import Not from './pages/Not'
+
+import Header from './components/section/Header';   //추가
+import Main from './components/section/Main';       //추가
+import Footer from './components/section/Footer';   //추가
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Main>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path="/today" element={<Today />} />
+                    <Route path="/developer" element={<Developer />} />
+                    <Route path="/webd" element={<Webd />} />
+                    <Route path="/website" element={<Website />} />
+                    <Route path="/gsap" element={<Gsap />} />
+                    <Route path="/port" element={<Port />} />
+                    <Route path="/youtube" element={<Youtube />} />
+                    <Route path='/channel/:channelId' element={<Channel />} />
+                    <Route path='/video/:videoId' element={<Video />} />
+                    <Route path='/search/:searchId' element={<Search />} />
+                    <Route path="*" element={<Not />} />
+                </Routes>
+            </Main>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
